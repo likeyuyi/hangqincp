0e3e77bb287fe2d19986b7df1856ae36a646eed4
likeyuyi <ysysjuan@gmail.com>
2019-05-13 18:26:35 +0800

    版本1.01

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import os
 import tkinter
 import tkinter.filedialog
+from multiprocessing import Process, Queue
 from time import sleep
 
 import mss
@@ -10,8 +11,6 @@ import pytesseract
 import xlwt
 from PIL import Image
 from PIL import ImageGrab
-from pyecharts import options as opts
-from pyecharts.charts import Line
 
 
 class MyCapture:
@@ -105,6 +104,7 @@ def buttonCaptureClick():
     global fullbuttom, fullleft, fullright, fulltop, shuju
     print(w.selectPosition)
     area.append(w.selectPosition)
+    # 计算最大需要截图区域
     for item in area:
         # print(item[0])
         fullleft = item[0] if fullleft > item[0] else fullleft
@@ -118,90 +118,52 @@ def buttonCaptureClick():
     os.remove(filename)
 
 
-def buttonzhuatu():
+def grab(queue, fullleft, fulltop, fullright, fullbuttom, Interval=0.2, numbers=1000, ):
+    monitor = {"top": fulltop, "left": fullleft, "width": fullright - fullleft, "height": fullbuttom - fulltop}
     with mss.mss() as sct:
-        global fullbuttom, fullleft, fullright, fulltop, shuju, code, code1
-        monitor = {"top": fulltop, "left": fullleft, "width": fullright - fullleft, "height": fullbuttom - fulltop}
-        n = 300
-        # 截图
-        for i in range(n):
-            output = str(i) + ".png"
-            sct_img = sct.grab(monitor)
-            mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
-            sleep(0.2)
-        # 计算有效数据区
-        for item in area:
-            shuju.append((item[0] - fullleft, item[2] - fulltop, item[1] - fullleft, item[3] - fulltop))
-        print(shuju)
-        # 识别数据
-        for i in range(n):
-            output = str(i) + ".png"
-            img = Image.open(output)
-            code = []
+        for i in range(numbers):
+            queue.put(sct.grab(monitor))
+            sleep(Interval)
 
-            code.clear()
-            '''
-            print(i,code)
-            img = img.convert('RGBA')  # 转换为RGBA
-            pix = img.load()
-            
-            for y in range(img.size[1]):  # 二值化处理，这个阈值为R=95，G=95，B=95
-                for x in range(img.size[0]):
-                    if  pix[x, y][1] > 13 or pix[x, y][2] > 13:
-                        pix[x, y] = (255, 255, 255, 255)
 
-                    else:
-                        pix[x, y] = (0, 0, 0, 255)
-            img.save('a'+output)
-            '''
+def identify(queue, queue1, area, fullleft, fulltop, fullright, fullbuttom, numbers=1000):
+    shuju = []
 
-            # print(shuju)
-            code.insert(0, str(i))
-            print(code)
-            # 选定区域识别
-            m = 0
-            for item in shuju:
+    for item in area:
+        shuju.append((item[0] - fullleft, item[2] - fulltop, item[1] - fullleft, item[3] - fulltop))
+    for i in range(numbers):
+        img = queue.get()
+        img1 = Image.frombytes("RGB", img.size, img.bgra, "raw", "BGRX")
+        m = 0
+        code = []
 
-                region = img.crop(item)
+        for item in shuju:
 
-                if m == 0:
-                    code.append(pytesseract.image_to_string(region, config='-psm 7 sfz', lang='new'))
-                    m = m + 1
-                else:
-                    code.append(pytesseract.image_to_string(region, config='-psm 7 sfz', lang='bh'))
-                    m = 0
-            # print(i,code)
-            print(code)
-            code1.append(code)
-            print(code1)
-        print(code1)
-        B1 = []
-        A1 = []
-        A2 = []
-        code2 = []
-        sssw = ""
-        for item in code1:
-            print(item)
-            if (item[1] != item[2] or item[1] != sssw):
-                B1.append(item[0])
-                A1.append(item[1])
-                A2.append(item[2])
-                code2.append(item)
-                sssw = item[1]
-        line = Line(init_opts=opts.InitOpts(width="1200px", height="800px"))
+            region = img1.crop(item)
 
-        line.set_global_opts(xaxis_opts=opts.AxisOpts(is_scale=True),
-                             yaxis_opts=opts.AxisOpts(is_scale=True),
-                             title_opts=opts.TitleOpts(title="行情快慢的比较"),
-                             datazoom_opts=[opts.DataZoomOpts(is_show=True)],
-                             toolbox_opts=opts.ToolboxOpts(is_show=True))
+            if m == 0:
+                code.append(pytesseract.image_to_string(region, config='-psm 7 sfz', lang='new'))
+                queue1.put(region)
+                m = m + 1
+            else:
+                code.append(pytesseract.image_to_string(region, config='-psm 7 sfz', lang='bh'))
+                queue1.put(region)
+                m = 0
+        # code1.append(code)
+        print(code)
 
-        line.add_xaxis(B1)
-        line.add_yaxis("同花顺", A1)
-        line.add_yaxis("通达信", A2)
 
-        line.render()
-        data_write("data.xls", code2)
+
+def buttonzhuatu():
+    global area, fullleft, fulltop, fullright, fullbuttom, code1
+    queue = Queue()
+    queue1 = Queue()
+    Process(target=grab, args=(queue, fullleft, fulltop, fullright, fullbuttom)).start()
+    Process(target=identify, args=(queue, queue1, area, fullleft, fulltop, fullright, fullbuttom)).start()
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -210,6 +172,7 @@ if __name__ == "__main__":
     fulltop = 99999
     fullright = 0
     fullbuttom = 0
+
     shuju = []
     code1 = []
     # 创建tkinter主窗口
